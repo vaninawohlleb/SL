c5e2aa2cc2b6913b4d48830c7fef69fe6852e180
Owner <vtsoneva@gmail.com>
2020-09-17 16:02:33 +0300

    latest stage

--- uploads/cache/82dcde5a3758d5fa443a2781dae525b0247f7086.php
+++ uploads/cache/82dcde5a3758d5fa443a2781dae525b0247f7086.php
@@ -4,14 +4,22 @@
     </div>
     <div class="team-member__info">
       <h6><?php echo e($member->title); ?></h6>
-      <p>Title: <?php echo e($member->business_title); ?></p>
-      <p>Email: <?php echo e($member->email); ?></p>
-    </div>
-    <?php if(!is_home()): ?>
-      <p class="team-member__bio">
-        <?php echo $member->bio; ?>
+      <p class="team-member__meta">
+        <img src="<?= App\asset_path('images/business-title.svg'); ?>" />
+        <?php echo e($member->business_title); ?>
 
       </p>
+      <p class="team-member__meta">
+        <img src="<?= App\asset_path('images/email.svg'); ?>" />
+        <?php echo e($member->email); ?>
+
+      </p>
+    </div>
+    <?php if(!is_home()): ?>
+      <div class="team-member__bio">
+        <?php echo $member->content; ?>
+
+      </div>
     <?php endif; ?>
   </li>
   
