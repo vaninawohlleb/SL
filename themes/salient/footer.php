45ebbd12bec624d373d6e9c19b611756bebe450c
Owner <vtsoneva@gmail.com>
2020-09-25 12:19:30 +0300

    footer

--- themes/salient/footer.php
+++ themes/salient/footer.php
@@ -17,7 +17,27 @@ $header_format  = ( !empty($nectar_options['header_format']) ) ? $nectar_options
 ?>
 
 <div id="footer-outer" <?php nectar_footer_attributes(); ?>>
-	
+	<section class="donate">
+		<a href="https://giving.massgeneral.org/donate/?re_fund=281410&donation_designated=Y&donation_designation=Dr.%20Shuhan%20He%27s%20Significance%20Lab" target="_blank">
+			<svg height="180" width="180">
+				<circle class="circle" cx="100" cy="100" r="90" stroke-width="1" />
+			</svg>
+			<svg>
+				<circle class="circle" cx="100" cy="100" r="83" stroke-width="1" />
+			</svg>
+			<svg>
+				<circle class="circle" cx="100" cy="100" r="92" stroke-width="1" />
+			</svg>
+			<svg>
+				<circle class="circle" cx="100" cy="100" r="78" stroke-width="1" />
+			</svg>
+			<svg>
+				<circle class="circle" cx="100" cy="100" r="75" stroke-width="1" />
+			</svg>
+			<h6>Donate to Scientific Labs</h6>
+		</a>
+	</section>
+
 	<?php
 	
 	get_template_part( 'includes/partials/footer/call-to-action' );
